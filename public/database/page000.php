0d50a8eb0342015429474b45452839a55c83d6ad
Duncan Smith <duncansmith@Duncans-Air.cust.communityfibre.co.uk>
2024-01-20 08:11:05 +0000

    Start dev

--- public/database/page000.php
+++ public/database/page000.php
@@ -2,12 +2,24 @@
 
 $page000 = [
 	0 => [
-		'title' => 'P1',
+		'title' => 'Untitled 2017',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 12LARGE.jpg',
 	],
 	1 => [
-		'title' => 'P2',
+		'title' => 'Untitled 2017',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 2LARGE.jpg',
+	],
+	2 => [
+		'title' => 'Untitled 2017',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 4LARGE.jpg',
+	],
+	3 => [
+		'title' => 'Untitled 2017',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 5LARGE.jpg',
+	],
+	4 => [
+		'title' => 'Untitled 2017',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 9LARGE.jpg',
 	] 
 ];
 
