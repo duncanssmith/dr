65ee27f26de683feedb5609d2f987e745a289a17
Duncan Smith <duncansmith@Duncans-Air.cust.communityfibre.co.uk>
2024-01-26 11:28:33 +0000

    Added new images to page 1

--- public/database/page000.php
+++ public/database/page000.php
@@ -2,25 +2,63 @@
 
 $page000 = [
 	0 => [
-		'title' => 'Untitled 2017',
+		'title' => 'Set 4 - 12',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 12LARGE.jpg',
 	],
 	1 => [
-		'title' => 'Untitled 2017',
+		'title' => 'Set 4 - 2',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 2LARGE.jpg',
 	],
 	2 => [
-		'title' => 'Untitled 2017',
+		'title' => 'Set 4 - 4',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 4LARGE.jpg',
 	],
 	3 => [
-		'title' => 'Untitled 2017',
+		'title' => 'Set 4 - 5',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 5LARGE.jpg',
 	],
 	4 => [
-		'title' => 'Untitled 2017',
+		'title' => 'Set 4 - 9',
 		'thumb' => './images/images/Paintings/2017/4/Set 4 - 9LARGE.jpg',
-	] 
+	],
+	5 => [
+		'title' => 'Set 4 - 13',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 13LARGE.jpg',
+	], 
+	6 => [
+		'title' => 'Set 4 - 13',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 14LARGE.jpg',
+	],
+	7 => [
+		'title' => 'Set 4 - 3',
+		'thumb' => './images/images/Paintings/2017/4/Set 4- 3LARGE.jpg',
+	],
+	8 => [
+		'title' => 'Set 4 - 9',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 9LARGE.jpg',
+	],
+	9 => [
+		'title' => 'Set 4 - 1',
+		'thumb' => './images/images/Paintings/2017/4/Set 4- 1 2017LARGE.jpg',
+	],
+	10 => [
+		'title' => 'Set 4 - 15',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 -15LARGE.jpg',
+	],
+	11 => [
+		'title' => 'Set 4 - 11',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 11LARGE.jpg',
+	],
+	12 => [
+		'title' => 'Set 4 - 10',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 10LARGE.jpg',
+	],
+	13 => [
+		'title' => 'Set 4 - 6',
+		'thumb' => './images/images/Paintings/2017/4/Set 4 - 6LARGE.jpg',
+	],
+	14 => [
+		'title' => 'Set 4 - 7',
+		'thumb' => './images/images/Paintings/2017/4/set 4 - 7LARGE.jpg',
+	]
 ];
-
-
